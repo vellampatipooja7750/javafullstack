1bed2030d8adfb31bd70841e24e4ac3948d02188
vellampatipooja7750 <vellampatipooja@gmail.com>
2025-12-17 23:11:16 +0530

    Add files via upload

---
+++ Day 3/PrimeOrNot.java
@@ -0,0 +1,16 @@
+class PrimeOrNot{
+    public static void main(String[] args) {
+        int n = 7;
+        int count = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0)
+                count++;
+        }
+
+        if(count == 2)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not Prime");
+    }
+}
